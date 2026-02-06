4e3cc4bdba75b9651b27a4fc1fe5c768230a8dd9
Salla0905 <bruna.salla@germinare.org.br>
2026-02-06 08:58:03 -0300

    criação de splash screen, telas de opção, esqueceu a senha e cadastro

---
+++ app/src/main/java/com/example/escolaboaparacachorro/Login.java
@@ -0,0 +1,54 @@
+package com.example.escolaboaparacachorro;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import org.w3c.dom.Text;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        TextInputEditText email = findViewById(R.id.email);
+        TextInputEditText senha = findViewById(R.id.senha);
+        Button bt_confirmar = findViewById(R.id.confirmar);
+        TextView esqueceuASenha = findViewById(R.id.textView4);
+
+        esqueceuASenha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this, EsqueceuASenha.class));
+            }
+        });
+
+        bt_confirmar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent();
+//                intent.putExtra("Email", email);
+//                intent.putExtra("Senha", senha);
+//                startActivity(intent);
+            }
+        });
+
+    }
+}
