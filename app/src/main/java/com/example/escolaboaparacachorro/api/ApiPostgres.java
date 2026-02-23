83ab39df1f389b12144e6f595ed01954b437098c
juliaporf <157625969+juliaporf@users.noreply.github.com>
2026-02-22 21:21:42 -0300

    adicionando adapters, models, e novas features

--- app/src/main/java/com/example/escolaboaparacachorro/api/ApiPostgres.java
+++ app/src/main/java/com/example/escolaboaparacachorro/api/ApiPostgres.java
@@ -1,6 +1,7 @@
 package com.example.escolaboaparacachorro.api;
 
 import com.example.escolaboaparacachorro.model.Cachorro;
+import com.example.escolaboaparacachorro.model.Notas;
 
 import java.util.List;
 
@@ -10,5 +11,9 @@ import retrofit2.http.GET;
 public interface ApiPostgres {
     @GET("cachorros")
     Call<List<Cachorro>> getCachorros();
+    //exemplo (aguardando api)
+
+    @GET("notas")
+    Call<List<Notas>> getNotas();
     //exemplo (aguardando api)
 }
--- app/src/main/java/com/example/escolaboaparacachorro/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/escolaboaparacachorro/ui/notifications/NotificationsFragment.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -16,7 +15,6 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.escolaboaparacachorro.Perfil;
-import com.example.escolaboaparacachorro.R;
 import com.example.escolaboaparacachorro.adapter.AumigosAdapter;
 import com.example.escolaboaparacachorro.api.ApiPostgres;
 import com.example.escolaboaparacachorro.databinding.FragmentNotificationsBinding;
